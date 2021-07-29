edddf8bcc0b6c114b993b16e577e4d9edc0379bb
Alasagas <900156@cognizant.com>
2021-07-29 12:32:13 -0700

    commit from work laptop

--- weather-app/app.js
+++ weather-app/app.js
@@ -54,5 +54,5 @@ geocode('Boston', (error, data) => {
 
 forecast(-75.7088, 44.1545, (error, data) => {
     console.log('Error', error)
-    console.log('Data', data)
+    console.log('Data - Test', data)
   })
